7781feb798adb3f01fcb6ab4834c11cc9186e6f0
Paola Stone <paola.stonemartinez@dell.com>
2019-09-07 17:36:20 -0400

    added default song display and updated to show just the first result

--- liri.js
+++ liri.js
@@ -11,13 +11,13 @@ var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify)
 
-// console.log(spotify)
-
 function concertThis(){
     axios.get("https://rest.bandsintown.com/artists/"+ userEntry + "/events?app_id=codingbootcamp").then(
+        
         function(response){
             // console.log(response.data)
             for (var i = 0; i < response.data.length; i++) {
+                console.log("   ")
                 console.log("Name of the venue: "+ response.data[i].venue.name);
                 console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
                 console.log("Date of the Event: " + moment(response.data[i].datetime).format("L"));
@@ -34,30 +34,47 @@ function concertThis(){
               console.log("Error", error.message);
             }
             console.log(error.config);
-          }
+        }
     )
 };
-// concertThis()
 
 function spotifyThisSong(){
-    spotify.search({type: "track", query: userEntry }, function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-        var songResults = data.tracks.items;
-        console.log(songResults)
-        for (var i = 0; i < songResults.length; i++){
-            console.log("Artist(s): " + songResults[i].artists[0].name);
-            console.log("Song Name: " + songResults[i].name);
-            console.log("Preview Link: " + songResults[i].preview_url);
-            console.log("Album: " + songResults[i].album.name);
+    if (!userEntry) {
+        spotify.search({type: "track", query: "The Sign" }, function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+
+            var songResults = data.tracks.items;
+                console.log("  ")
+                console.log("Artist(s): " + songResults[3].artists[0].name);
+                console.log("Song Name: " + songResults[3].name);
+                console.log("Preview Link: " + songResults[3].preview_url);
+                console.log("Album: " + songResults[3].album.name);
+                console.log("--------------------")
+               
+        });
+    } else{
+        spotify.search({type: "track", query: userEntry }, function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+            
+            var songResults = data.tracks.items;
+            // console.log(songResults)
+            console.log("   ")
+            console.log("Artist(s): " + songResults[0].artists[0].name);
+            console.log("Song Name: " + songResults[0].name);
+            console.log("Preview Link: " + songResults[0].preview_url);
+            console.log("Album: " + songResults[0].album.name);
             console.log("--------------------")
-        }
-    }); 
-};
+        });
 
-// spotifyThisSong()
+    }
 
+} 
+
+        
 function movieThis(){
 
     axios.get("http://www.omdbapi.com/?t=saw&y=&plot=short&apikey=trilogy")
