126678b77e07644ca41047a72c7692aee93e3f48
Paola Stone <paola.stonemartinez@dell.com>
2019-09-07 18:04:07 -0400

    added movietThis default and images

--- liri.js
+++ liri.js
@@ -76,21 +76,39 @@ function spotifyThisSong(){
 
         
 function movieThis(){
-
-    axios.get("http://www.omdbapi.com/?t=saw&y=&plot=short&apikey=trilogy")
-    .then(function(response) {
-        // console.log(response.data)
-        console.log("Title: " + response.data.Title);
-        console.log("Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-        console.log("Country where the movie was produced: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-    });
+    if (!userEntry) {
+        axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy")
+        .then(function(response) {
+            console.log("    ")
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country where the movie was produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("----------------")
+        });
+            
+    
+    } else{
+    axios.get("http://www.omdbapi.com/?t=" + userEntry + "&y=&plot=short&apikey=trilogy")
+        .then(function(response) {
+            console.log("    ")
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country where the movie was produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("----------------")
+        });
+    }
 }
-// movieThis()
+
 
 function doWhatItSays(){
 
